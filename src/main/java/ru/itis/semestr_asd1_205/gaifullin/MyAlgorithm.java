0db7b6b33edcb28ba73a2a0b576842d009f40d44
lyalkid <121316954+lyalkid@users.noreply.github.com>
2023-03-26 03:16:12 +0300

    добавил реализацию префикс функции

--- src/main/java/ru/itis/semestr_asd1_205/gaifullin/MyAlgorithm.java
+++ src/main/java/ru/itis/semestr_asd1_205/gaifullin/MyAlgorithm.java
@@ -1,4 +1,37 @@
 package ru.itis.semestr_asd1_205.gaifullin;
 
 public class MyAlgorithm {
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+            String string = "aabaaab";
+            System.out.print(prefixFunction(string));
+            System.out.print("");
+        }
+
+        public static int prefixFunction(String string){
+            int n = string.length();
+            int[] pi = new int[n];
+            for(int i = 1; i < n; ++i){
+                int j = pi[i-1];
+                while (j > 0 && string.charAt(i)!= string.charAt(j)){
+                    j = pi[j-1];
+                }
+                if (string.charAt(i) == string.charAt(j)){
+                    ++j;
+                }
+                pi[i] = j;
+            }
+            return max(pi);
+        }
+
+
+        public static int max(int[] array){
+            int maxim = -1;
+            for (int i = 0; i < array.length; ++i){
+                if (array[i] > maxim) {
+                    maxim = array[i];
+                }
+            }
+            return maxim;
+        }
 }
