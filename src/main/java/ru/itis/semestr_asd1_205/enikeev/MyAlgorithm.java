644f6a15a9762839f05f79b4d70fa4e767890b66
lyalkid <121316954+lyalkid@users.noreply.github.com>
2023-03-29 18:32:28 +0300

    удалил случайные файлы

--- src/main/java/ru/itis/semestr_asd1_205/enikeev/MyAlgorithm.java
+++
@@ -1,4 +0,0 @@
-package ru.itis.semestr_asd1_205.enikeev;
-
-public class MyAlgorithm {
-}
--- src/main/java/ru/itis/semestr_asd1_205/gainullova/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.itis.semestr_asd1_205.gainullova;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(123);
-    }
-}
