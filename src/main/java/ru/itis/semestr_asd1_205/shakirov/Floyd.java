8622d5a86086bdd2d7cb4b830a5653c1e053c990
Euwaq <euwaqq@gmail.com>
2023-04-06 11:58:35 +0300

    BugFix

--- src/main/java/ru/itis/semestr_asd1_205/shakirov/Floyd.java
+++ src/main/java/ru/itis/semestr_asd1_205/shakirov/Floyd.java
@@ -1,20 +1,31 @@
 package ru.itis.semestr_asd1_205.shakirov;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Floyd {
-int size;
-double [][] D0;
-double[][][] D;
+    static int count=0;
+
+
+    private double[][][] D;
+    private int size;
+    private double [][] D0;
+
+
     public Floyd(double[][] d0) {
         D0 = d0;
         size=d0.length;
         D=new double[size][][];
     }
-    public static double[][] newDn(double[][] d, int n){
+    public double[][] newDn(double[][] d, int n){
         int size=d.length;
         double[][] Dn=new double[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 Dn[i][j]=Double.min(d[n][j]+d[i][n],d[i][j]);
+                count++;
             }
         }
         return Dn;
@@ -22,7 +33,7 @@ double[][][] D;
 
     public void compute(){
         D[0]=newDn(D0,0);
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < size-1; i++) {
             D[i+1]=newDn(D[i],i+1);
         }
     }
