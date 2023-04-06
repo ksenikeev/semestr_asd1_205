37c26e8d4ee3e85f3c5216e9945b376c9e2ced28
Euwaq <euwaqq@gmail.com>
2023-04-06 13:02:46 +0300

    Уменьшил размер входных данных в 10 раз

--- src/main/java/ru/itis/semestr_asd1_205/shakirov/LightFloyd.java
+++ src/main/java/ru/itis/semestr_asd1_205/shakirov/LightFloyd.java
@@ -11,14 +11,15 @@ public class LightFloyd {
     public static void main(String[] args) {
         try (FileWriter writer = new FileWriter("Floyd.csv");
              BufferedReader reader=new BufferedReader(new FileReader("matrix.txt"))) {
-            for (int s = 100; s < 10001; s += 100) {
+            for (int s = 100; s < 1001; s += 10) {
                 //Заполнение матрицы
-                double[][] a = new double[s][s];
+                byte[][] a = new byte[s][s];
                 for (int i = 0; i < s; i++) {
                     for (int j = 0; j < s; j++) {
-                        double db= reader.read();
-                        if (db==32)db=Double.POSITIVE_INFINITY;
-                        a[i][j]=db;
+                        //double db= reader.read();
+                        //if (db==32)db=Double.POSITIVE_INFINITY;
+                        //a[i][j]=db;
+                        a[i][j]=(byte)reader.read();
                     }
                 }
 
@@ -33,18 +34,18 @@ public class LightFloyd {
         }
     }
 
-    public static double[][] compute(double[][] D){
+    public static byte[][] compute(byte[][] D){
         for (int i = 0; i < D.length; i++) {
             D=newDn(D,i);
         }
         return D;
     }
-    public static double[][] newDn(double[][] d, int n){
+    public static byte[][] newDn(byte[][] d, int n){
         int size=d.length;
-        double[][] Dn=new double[size][size];
+        byte[][] Dn=new byte[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                Dn[i][j]=Double.min(d[n][j]+d[i][n],d[i][j]);
+                Dn[i][j]=(byte) Math.min(d[n][j]+d[i][n],d[i][j]);
                 count++;
             }
         }
